d3ca47f03e137752a0554e7a87d31d15b250d978
Chris Walter <walt50@gmail.com>
2017-12-14 13:53:49 -0500

    First Commit

---
+++ PongSuper/GameOverScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameOverScene.swift
+//  PongSuper
+//
+//  Created by Christopher Walter on 12/13/17.
+//  Copyright © 2017 AssistStat. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let label = SKLabelNode(fontNamed: "Arial")
+        label.numberOfLines = 2
+        label.text = "GameOver\nYou:\(GameScene.playerScore) Computer:\(GameScene.computerScore) "
+        label.fontSize = 50
+        label.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        label.name = "label"
+        addChild(label)
+        
+        let playAgainButton = SKLabelNode(fontNamed: "Arial")
+        playAgainButton.text = "Play Again?"
+        playAgainButton.fontSize = 50
+        playAgainButton.position = CGPoint(x: frame.width / 2, y: frame.height * 0.2)
+        playAgainButton.name = "playAgainButton"
+        addChild(playAgainButton)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
+        
+        let myNodes = nodes(at: touchLocation)
+        
+        for node in myNodes {
+            if node.name == "playAgainButton" {
+                let gameScene = GameScene(fileNamed: "GameScene")
+                gameScene?.scaleMode = .aspectFill
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                view?.presentScene(gameScene!, transition: reveal)
+            }
+        }
+    }
+
+}
